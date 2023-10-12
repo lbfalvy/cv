0cb2d21971c3cf889a22102fd4dff7b7c2e8b49d
Lawrence Bethlenfalvy <lbfalvy@protonmail.com>
2023-10-12 23:25:01 +0100

    Removed unused declarations

--- src/Options.tsx
+++ src/Options.tsx
@@ -1,23 +1,7 @@
 import React from "react"
 import './Options.scss'
 
-function RadioFor({ name, value }: { name: string, value: string }): React.ReactElement {
-  const params = new URLSearchParams(window.location.search)
-  return <label>
-    <input
-      type='radio' name={name} value={value}
-      checked={params.get(name) == value}
-      onClick={() => {
-        params.set(name, value)
-        window.location.search = params.toString()
-      }}
-    />
-    {value}
-  </label>
-}
-
 export function Options(): React.ReactElement {
-  const params = new URLSearchParams(window.location.search)
   return <div className="Options">
     <h1>CV generator</h1>
     <p>
