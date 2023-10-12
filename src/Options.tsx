9ed778f458abf69807bba862c5b1f1e0533559af
Lawrence Bethlenfalvy <lbfalvy@protonmail.com>
2023-10-12 23:53:58 +0100

    added second page, improved explainer

--- src/Options.tsx
+++ src/Options.tsx
@@ -5,12 +5,12 @@ export function Options(): React.ReactElement {
   return <div className="Options">
     <h1>CV generator</h1>
     <p>
-      This isn't an on-line CV, it is a template for printing PDFs. If you try to print it,
-      this explanatory paragraph will disappear and the remaining document will fit perfectly
-      onto an A4 page.
+      This is a template for printing PDFs, not an on-line CV. If you try to print it, this
+      explanatory paragraph disappears, the remaining document fits perfectly on two A4 pages,
+      and the red unused space highlights disappear.
     </p>
     <p>
-      If there were variable options, such as sections that are only visible when
+      If there were variable options such as sections that are only visible when
       printing for a particular field of work, they would be displayed here.
     </p>
   </div>
--- src/Page2.tsx
+++ src/Page2.tsx
@@ -2,6 +2,8 @@ import "./Page2.scss";
 import { Spacer } from "./Section";
 
 export const Page2 = () => <main className="Page2" >
-  <h1>Hello!?</h1>
+  <header className="header">
+    <h1>Portfolio</h1>
+  </header>
   <Spacer />
 </main>
